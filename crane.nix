9ba75e6b09998cf74d750acdba4afddaf138a144
Atahan Yorgancı <atahanyorganci@hotmail.com>
2026-02-23 21:38:56 +0300

    feat: create a Nix flake for `brightness-cli`

---
+++ crane.nix
@@ -0,0 +1,65 @@
+{inputs, ...}: {
+  perSystem = {
+    pkgs,
+    craneLib,
+    self',
+    ...
+  }: let
+    inherit (pkgs) lib;
+    rustToolchainFor = p:
+      p.rust-bin.selectLatestNightlyWith (
+        toolchain:
+          toolchain.default.override {
+            extensions = ["rust-src" "rustfmt"];
+          }
+      );
+    rustToolchain = rustToolchainFor pkgs;
+    craneLibNightly = craneLib.overrideToolchain rustToolchainFor;
+    src = lib.cleanSourceWith {
+      src = ./.;
+      filter = path: type:
+        (craneLib.filterCargoSources path type)
+        || (lib.hasSuffix "wrapper.h" path);
+      name = "brightness-cli-src";
+    };
+    commonArgs = {
+      inherit src;
+      strictDeps = true;
+      buildInputs = [] ++ lib.optionals pkgs.stdenv.isDarwin [pkgs.libiconv];
+    };
+    cargoArtifacts = craneLibNightly.buildDepsOnly commonArgs;
+    individualCrateArgs =
+      commonArgs
+      // {
+        inherit cargoArtifacts;
+        inherit (craneLibNightly.crateNameFromCargoToml {inherit src;}) version;
+        doCheck = false;
+      };
+    brightness-cli = craneLibNightly.buildPackage (
+      individualCrateArgs
+      // {
+        pname = "brightness-cli";
+        src = src;
+      }
+    );
+  in {
+    checks = {
+      # Audit dependencies
+      "brightness-cli-audit" = craneLib.cargoAudit {
+        inherit src;
+        advisory-db = inputs.advisory-db;
+      };
+
+      # Audit licenses
+      "brightness-cli-deny" = craneLib.cargoDeny {
+        inherit src;
+      };
+    };
+    packages."brightness-cli" = brightness-cli;
+    devShells.default = craneLibNightly.devShell {
+      checks = self'.checks;
+      packages = [rustToolchain pkgs.cargo-watch];
+      RUST_SRC_PATH = "${rustToolchain.passthru.availableComponents.rust-src}/lib/rustlib/src/rust/library";
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Brightness CLI";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        ./crane.nix
+      ];
+      systems = ["aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        system,
+        self',
+        ...
+      }: let
+        pkgs = import self.inputs.nixpkgs {
+          inherit system;
+          overlays = [self.inputs.rust-overlay.overlays.default];
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+          };
+        };
+        craneLib = self.inputs.crane.mkLib pkgs;
+      in {
+        _module.args = {
+          inherit pkgs craneLib;
+        };
+        packages.default = self'.packages."brightness-cli";
+      };
+      flake = {};
+    };
+}
